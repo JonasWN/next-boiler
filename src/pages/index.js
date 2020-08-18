34fc67448a37ff5bc178d1ca430d32e197431dda
Jonas Wessman Nielsen <5615825@RTS-365.dk>
2020-08-18 10:16:23 +0200

    Setup template

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout"
+
+function HomePage() {
+  return (
+    <Layout title="Home">
+      <main>
+        <h1>hey</h1>
+      </main>
+    </Layout>
+  )
+}
+
+export default HomePage
---
+++ src/styles/theme.js
@@ -0,0 +1,8 @@
+export const Theme = {
+    colors: {
+
+    },
+    fontSizes: {
+
+    },
+  }
